91cdd589753e8bc71c1b4fdcb0da6617a26e1b97
Khalid Shaikh <k@ruby.run>
2018-01-31 23:13:08 -0800

    Validates letters and numbers for sku.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
     belongs_to :category
     has_one :order
+    validates :sku, format: { with: /\A[a-zA-Z0-9\s]+\z/i, message: "can only contain letters and numbers." }
 end
