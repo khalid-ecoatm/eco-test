073a6de477dacc9b632e41ec1327a218f99d6f6f
Khalid Shaikh <k@ruby.run>
2018-01-31 22:21:37 -0800

    Created models & migrations for eco-test

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'root#index'
+  resources :products
+  resources :orders
 end
